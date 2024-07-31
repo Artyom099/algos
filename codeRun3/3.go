3817a18152852f84d2b31c117f7009f12947fea0
Artyom Golubev <artgolubev@bk.ru>
2024-07-31 22:16:08 +0300

    add code run

--- codeRun3/3.go
+++ codeRun3/3.go
@@ -11,29 +11,39 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	numbersStr, _ := reader.ReadString('\n')
-	numbersStr = strings.TrimSpace(numbersStr)
-	numbersStrs := strings.Split(numbersStr, " ")
+
+	fmt.Print("Введите кнопки: ")
+	buttonsStr, _ := reader.ReadString('\n')
+	buttonsStr = strings.TrimSpace(buttonsStr)
+	buttonsStrs := strings.Split(buttonsStr, " ")
+	fmt.Println("buttonsStrs", buttonsStrs)
+
+	fmt.Print("Введите число: ")
+	nStr, _ := reader.ReadString('\n')
+	nStr = strings.TrimSpace(nStr)
+	nSlice := strings.Split(nStr, "")
+	fmt.Println("nSlice: ", nSlice, "len: ", len(nSlice))
 
 	numbersArr := make([]int, 3)
 	for i := 0; i < 3; i++ {
-		numbersArr[i], _ = strconv.Atoi(numbersStrs[i])
+		numbersArr[i], _ = strconv.Atoi(buttonsStrs[i])
 	}
+	fmt.Println("numbersArr-кнопки: ", numbersArr)
 
-	nStr, _ := reader.ReadString('\n')
-	nStr = strings.TrimSpace(nStr)
-	nSlice := strings.Split(numbersStr, " ")
-	fmt.Println(numbersArr)
-	fmt.Println(nSlice)
+	nArr := make([]int, len(nSlice))
+	for i := 0; i < len(nSlice); i++ {
+		nArr[i], _ = strconv.Atoi(nSlice[i])
+	}
+	fmt.Println("nArr-цифры числа", nArr)
 
 	var buttonsForAdd int = 0
 
-	for n := range nSlice {
+	for _, n := range nArr {
 		hasButton := slices.Contains(numbersArr, n)
 		fmt.Println(hasButton)
 		if !hasButton {
 			buttonsForAdd++
 		}
 	}
-	fmt.Println(buttonsForAdd)
+	fmt.Println("buttonsForAdd", buttonsForAdd)
 }
